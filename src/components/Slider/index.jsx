0adebb5546474611b289ead2f7606836d9251edf
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-11-30 16:55:04 +0300

    update project

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SliderDate from './SliderData.json'
+import './Slider.scss'
+
+export default function Slider() {
+    return (
+        <div className="slider">
+            <img src="" alt="" />
+        </div>
+    )
+}
