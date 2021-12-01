aeba0599fae581a892e12c094b27b4e90bc7e085
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-01 10:50:25 +0300

    Update index.jsx

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import SliderDate from './SliderData.json'
+import { SliderData } from './SliderData'
+
+import arrowPrev from '../../assets/img/arrow-prev-white.png'
+import arrowNext from '../../assets/img/arrow-next-white.png'
+
 import './Slider.scss'
 
-export default function Slider() {
+export default function Slider({ slides }) {
+
+
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+
     return (
-        <div className="slider">
-            <img src="" alt="" />
-        </div>
+        <section className='slider'>
+            <img src={arrowPrev} alt="prevSlide" className='prev-arrow' onClick={prevSlide} />
+            <img src={arrowNext} alt="nextSlide" className='next-arrow' onClick={nextSlide} />
+            {SliderData.map((slide, index) => {
+                return (
+                    <div
+                        className={index === current ? 'slide active' : 'slide'}
+                        key={index}
+                    >
+                        {index === current && (
+                            <img src={slide.image} alt='travel image' className='image' />
+                        )}
+                    </div>
+                );
+            })}
+        </section>
+
     )
 }
+
