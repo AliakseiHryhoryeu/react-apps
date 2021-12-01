eebfb79d828b3ab5dd6b603be7a58026dc4c535f
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-01 19:20:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import Slider from './components/Slider'
+
 import img1 from './assets/img/cat-1.jpg';
 import img2 from './assets/img/cat-2.jpg';
 import img3 from './assets/img/cat-3.jpg';
@@ -9,8 +10,8 @@ import img6 from './assets/img/cat-6.jpg';
 import img7 from './assets/img/cat-7.jpg';
 import img8 from './assets/img/cat-8.jpg';
 
-
 function App() {
+  
   const sliderDB = [img1,img2,img3,img4,img5,img6,img7,img8]
 
   return (
