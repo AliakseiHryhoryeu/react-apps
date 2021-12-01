572e69e19178aa890fcf61745d9f797059f248c4
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-01 10:50:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React,{Fragment} from 'react'
+import React, { Fragment } from 'react'
+import Slider from './components/Slider'
+import { SliderData } from './components/Slider/SliderData'
 
 function App() {
   return (
     <Fragment >
-
+      <Slider slides={SliderData} />
     </Fragment>
   );
 }
