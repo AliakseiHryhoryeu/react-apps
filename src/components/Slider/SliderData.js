d7d95ecaa6c017cd18548e6184712d7d47174b3a
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-01 10:50:28 +0300

    Create SliderData.js

---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,36 @@
+import img1 from '../../assets/img/cat-1.jpg'
+import img2 from '../../assets/img/cat-2.jpg'
+import img3 from '../../assets/img/cat-3.jpg'
+import img4 from '../../assets/img/cat-4.jpg'
+import img5 from '../../assets/img/cat-5.jpg'
+import img6 from '../../assets/img/cat-6.jpg'
+import img7 from '../../assets/img/cat-7.jpg'
+import img8 from '../../assets/img/cat-8.jpg'
+
+export const SliderData = [
+  {
+    image:{img1}
+  },
+  {
+    image:{img2}
+  },
+  {
+    image:{img3}
+  },
+  {
+    image:{img4}
+  },
+  {
+    image:{img5}
+  },
+  {
+    image:{img6}
+  },
+  {
+    image:{img7}
+  },
+  {
+    image:{img8}
+  }
+  ];
+  
